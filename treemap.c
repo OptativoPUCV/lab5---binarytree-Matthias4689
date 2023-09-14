15edba056a873dbb92c58e9cffcc76e741679aa1
Replit user <>
2023-09-14 17:57:09 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -73,11 +73,11 @@ Pair * searchTreeMap(TreeMap * tree, void* key) {
   TreeNode* aux = tree->root;
 
   while(aux != NULL){
-    if(is_equal(tree,key,aux->pair->key)){
+    if(key == aux->pair->key){
       tree->current = aux;
       return aux->pair;
     }
-    else if(tree->lower_than(key, aux->pair->key)) aux = aux->left;
+    else if(key > aux->pair->key) aux = aux->left;
 
     else  aux = aux->right;
   }
